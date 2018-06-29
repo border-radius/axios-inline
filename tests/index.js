2ff200c7a4934d5e5a2d2dc2ab6cb23587fcf5c8
border-radius <pharshmaster@gmail.com>
2018-06-29 20:10:08 +0300

    test iframe embeding

--- tests/index.js
+++ tests/index.js
@@ -11,6 +11,16 @@ function btoa (str) {
     return new Buffer(str).toString('base64');
 }
 
+function atob (str) {
+    return Buffer.from(str, 'base64').toString();
+}
+
+function getEmbedded (input) {
+    return input.toString()
+    .match(/data:[^;]+;base64,([^"']+)/g)
+    .map(embed => embed.slice(embed.indexOf(',')));
+}
+
 describe('Axios-inline', () => {
     let server;
 
@@ -82,6 +92,19 @@ describe('Axios-inline', () => {
         const pngContentBase64 = btoa(pngContent);
         request(url).then(response => {
             assert.equal(response.data.toString().indexOf(pngContentBase64) > -1, true);
+            done();
+        }).catch(done);
+    });
+
+    it('should get html with iframe embedded', done => {
+        const url = [TEST_WEBSERVER_ADDRESS, 'iframe.html'].join('/');
+        const iframePath = path.join(__dirname, 'pages/empty.html');
+        const iframeContent = fs.readFileSync(iframePath, { encoding: 'utf8' }).replace(/\s+/g, '');
+        request(url).then(response => {
+            const embeds = getEmbedded(response.data);
+            assert.equal(embeds.length, 1);
+            const embeddedIframe = atob(embeds[0]).replace(/\s+/g, '');
+            assert.equal(iframeContent, embeddedIframe);
             done();
         }).catch(done);
     });
